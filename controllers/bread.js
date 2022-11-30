2da4a8907f775ea7d69679720cdabef467d53f5a
Melik Melikyan <110069520+MHMelikyan@users.noreply.github.com>
2022-11-29 20:56:35 -0500

    second commit

--- controllers/bread.js
+++ controllers/bread.js
@@ -4,7 +4,9 @@ const Bread = require('../models/bread')
 
 //GET: all the bread
 router.get('/',(req, res)=>{
-    res.send(Bread)
+    res.render('index', {
+        breads: Bread
+    })
 })
 
 //GET: get a bread by its index
